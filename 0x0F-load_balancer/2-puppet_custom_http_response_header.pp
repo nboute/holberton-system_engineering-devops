f8d75d25f0e2c68a36a52ecabaa5f58cfba31928
Nicolas Boute <4249@holbertonschool.com>
2022-08-22 16:55:01 +0200

    added package requirement for a modification

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,10 +10,11 @@ package {'nginx':
 }
 
 file_line { 'add custom header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'add_header X-Served-By $hostname;',
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
